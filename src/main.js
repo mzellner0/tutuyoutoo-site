257a8c900f0cc0ba95b5d09c35fd28b1a773417b
Maud <maud-externe.zellner@stereograph.fr>
2026-04-15 13:49:51 +0200

    fix 404

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ const app = createApp(App);
 
 app.config.globalProperties.$tr = Translation;
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
+
 export default app
   .use(store)
   .use(router)
